8dd928ecb2b42b873a5b1e0d8a1abee7e585ae78
byrneb36 <autarch123-allhail@yahoo.ie>
2016-01-18 13:19:00 +0000

    Part implement full word data feature

--- app/src/main/java/com/dcu/superword_a_day/MyFragment.java
+++ app/src/main/java/com/dcu/superword_a_day/MyFragment.java
@@ -6,7 +6,10 @@ import java.io.ObjectInputStream;
 import java.util.IllegalFormatCodePointException;
 import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.NavigableMap;
 import java.util.Set;
+import java.util.SortedMap;
+import java.util.TreeMap;
 
 import android.content.BroadcastReceiver;
 import android.content.Context;
@@ -30,7 +33,8 @@ public class MyFragment extends Fragment {
     private int mNum;
     private String mSource;
     private View my_fragment_view;
-    private String thisFragmentsWord;
+    private String mWord, mDefinition1, mDefinition2, mDefSource1, mDefSource2, mExample1, mExampleSource1, mExample2, mExampleSource2,
+            mOrigin;
 
     private BroadcastReceiver mMessageReceiver = new BroadcastReceiver() {
 
@@ -40,7 +44,7 @@ public class MyFragment extends Fragment {
             Log.d("receiver", "Got message: " + wordsWithPercentages.toString());
             WordViewer.percentages_received_flag = true;
             // display the percentage associated with the current fragment
-            int wordPercentage = wordsWithPercentages.getInt(thisFragmentsWord);
+            int wordPercentage = wordsWithPercentages.getInt(mWord);
             ProgressBar percLoadingBar = (ProgressBar) my_fragment_view.findViewById(R.id.percentageLoadingBar);
             percLoadingBar.setVisibility(View.GONE);
             TextView percTextView = (TextView) my_fragment_view.findViewById(R.id.percentageTextView);
@@ -92,7 +96,8 @@ public class MyFragment extends Fragment {
         
     	final SlidingDrawer s = (SlidingDrawer) my_fragment_view.findViewById(R.id.slidingDrawer1);	
 
-    	LinkedList<String> wordAndDefinition;
+    	//LinkedList<String> wordAndDefinition;
+        TreeMap fullWordData;
 
         ((TextView)tv).setText("" + mNum);
         //+ "/" + temp.size());
@@ -101,7 +106,7 @@ public class MyFragment extends Fragment {
         if((Constants.SOURCE_TODAYS_WORDS).equals(mSource)) {
             Log.i("MyFragment", "inside diff perc check");
             if(WordViewer.percentages_received_flag == true) {
-                displayPercentage(WordViewer.wordsAndDefinitionsList);
+                displayPercentage(WordViewer.fullWordDataList);
             }
             else {
                 // otherwise, try registering the broadcast receiver
@@ -119,7 +124,7 @@ public class MyFragment extends Fragment {
             Log.i("MyFragment", "diff perc check: source not today's words");
             File f = new File("/data/data/com.dcu.superword_a_day/shared_prefs/" + Constants.DIFFICULTY_PERCENTAGES_FILE + ".xml");
             if(f.exists()) {
-                displayPercentage(WordViewer.wordsAndDefinitionsList);
+                displayPercentage(WordViewer.fullWordDataList);
             }
             else {
                 // otherwise, just remove the progress bar
@@ -130,8 +135,38 @@ public class MyFragment extends Fragment {
 
         /***************************************************************************************************************/
 
-    	wordAndDefinition = WordViewer.wordsAndDefinitionsList.get(mNum);
-        
+    	fullWordData = WordViewer.fullWordDataList.get(mNum);
+
+        // displaying word data
+        if(fullWordData != null) {
+            TextView wordText = (TextView) my_fragment_view.findViewById(R.id.wordText);
+            TextView definition1Text = (TextView) my_fragment_view.findViewById(R.id.definition1Text);
+            TextView definitionSource1Text = (TextView) my_fragment_view.findViewById(R.id.definitionSource1Text);
+            TextView definition2Text = (TextView) my_fragment_view.findViewById(R.id.definition2Text);
+            TextView definitionSource2Text = (TextView) my_fragment_view.findViewById(R.id.definitionSource2Text);
+            TextView example1Text = (TextView) my_fragment_view.findViewById(R.id.example1Text);
+            TextView exampleSource1Text = (TextView) my_fragment_view.findViewById(R.id.exampleSource1Text);
+            TextView example2Text = (TextView) my_fragment_view.findViewById(R.id.example2Text);
+            TextView exampleSource2Text = (TextView) my_fragment_view.findViewById(R.id.exampleSource2Text);
+            TextView originText = (TextView) my_fragment_view.findViewById(R.id.originText); // i.e. etymology
+
+            mWord = (String) fullWordData.get("word");
+            wordText.setText(mWord);
+            Log.i("MyFragment", "WORD: " + mWord);
+            mDefinition1 = (String) fullWordData.get("definitionSense1");
+            SortedMap wordDefs = getByPrefix(fullWordData, "definitionSense1");
+            mDefinition1 = (String) wordDefs.remove(wordDefs.firstKey());
+            while(!wordDefs.isEmpty()) {
+                // create new textviews for additional senses in the definition
+
+                mDefinition1 = mDefinition1 + wordDefs.remove(wordDefs.firstKey()) + "/n";
+            }
+            definition1Text.setText(mDefinition1);
+
+
+
+        }
+        /*
         if(wordAndDefinition != null) {
 	        TextView word = (TextView) my_fragment_view.findViewById(R.id.word1);
             thisFragmentsWord = wordAndDefinition.get(0).toString();
@@ -148,45 +183,51 @@ public class MyFragment extends Fragment {
 	        	abbrev.append(wordAndDefinition.get(i).toString());
 	        }
         }
+        */
 
-        
-        final GestureDetector gesture = new GestureDetector(getActivity(),
-                new GestureDetector.SimpleOnGestureListener() {
-        	
-	            @Override
-	            public boolean onDown(MotionEvent e) {
-	                return true;
-	            }
-	            
-                @Override
-                public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
-                	System.out.println("Syro E1: " +e1+" E2: " +e2);
-                	// determine if the intention is to swipe vertically rather than horizontally
-                	if(Math.abs(e1.getY() - e2.getY()) > Math.abs(e1.getX() - e2.getX())) {
-                		// determine whether it's an up or down swipe
-                		if(e2.getY() < e1.getY()) {
-                        	s.setVisibility(View.VISIBLE); // set to visible
-                        	s.animateOpen();
-                		}
-                		else {
-                			s.animateClose();
-                		}
-                		return true;
-                	}
-                	else {
-	                	s.setVisibility(View.INVISIBLE); // set to invisible
-	                	s.close(); // make sure the sliding drawer is always closed when a fragment is swiped to
-                    	return false;
-                	}
-                }
-                });
+        // don't allow the user to reveal the sliding drawer "start test" button when they are coming from the Word Archive
+        if(!(Constants.SOURCE_WORD_ARCHIVE_ACTIVITY).equals(mSource)) {
+            final GestureDetector gesture = new GestureDetector(getActivity(),
+                    new GestureDetector.SimpleOnGestureListener() {
 
+                        @Override
+                        public boolean onDown(MotionEvent e) {
+                            return true;
+                        }
+
+                        @Override
+                        public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+                            System.out.println("Syro E1: " +e1+" E2: " +e2);
+                            // determine if the intention is to swipe vertically rather than horizontally
+                            if(Math.abs(e1.getY() - e2.getY()) > Math.abs(e1.getX() - e2.getX())) {
+                                // determine whether it's an up or down swipe
+                                if(e2.getY() < e1.getY()) {
+                                    s.setVisibility(View.VISIBLE); // set to visible
+                                    s.animateOpen();
+                                }
+                                else {
+                                    s.animateClose();
+                                }
+                                return true;
+                            }
+                            else {
+                                s.setVisibility(View.INVISIBLE); // set to invisible
+                                s.close(); // make sure the sliding drawer is always closed when a fragment is swiped to
+                                return false;
+                            }
+                        }
+                    });
             my_fragment_view.setOnTouchListener(new View.OnTouchListener() {
                 @Override
                 public boolean onTouch(View v, MotionEvent event) {
                     return gesture.onTouchEvent(event);
                 }
             });
+        }
+
+
+
+
         return my_fragment_view;
     }
 
@@ -204,11 +245,11 @@ public class MyFragment extends Fragment {
         super.onDestroy();
     }
 
-    private void displayPercentage(LinkedList<LinkedList<String>> temp) {
+    private void displayPercentage(LinkedList<TreeMap> fullWordDataList) {
         SharedPreferences percentagesFile = getActivity().getApplicationContext()
                 .getSharedPreferences(Constants.DIFFICULTY_PERCENTAGES_FILE, 0);
         // get the percentage associated with the current fragment's word
-        int wordPercentage = percentagesFile.getInt((String) temp.get(mNum).get(0), -1);
+        int wordPercentage = percentagesFile.getInt((String) fullWordDataList.get(mNum).get("word"), -1);
         // remove the progress bar and display the percentage if it's available
         ProgressBar percLoadingBar = (ProgressBar) my_fragment_view.findViewById(R.id.percentageLoadingBar);
         percLoadingBar.setVisibility(View.GONE);
@@ -217,5 +258,9 @@ public class MyFragment extends Fragment {
             percTextView.setText("" + wordPercentage);
             percTextView.setVisibility(View.VISIBLE);
         }
+    }
+
+    private static SortedMap<String, Object> getByPrefix(NavigableMap<String, Object> myMap, String prefix) {
+        return myMap.subMap(prefix, prefix + Character.MAX_VALUE);
     }
 }
